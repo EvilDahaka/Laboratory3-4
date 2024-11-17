3566c56ad0a225afb4854430794b7ef8fe6cdc44
EvilDahaka <davidlesho100608@gmail.com>
2024-11-17 22:43:34 +0200

    added register

--- app.py
+++ app.py
@@ -76,6 +76,10 @@ def feedback():
 def cart():
     return render_template('cart.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
